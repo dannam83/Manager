0ed7b2f3e62b67a2e404d4379eded9d9c536110c
Daniel Nam <dannam83@gmail.com>
2018-11-01 19:56:53 -0400

    setup login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
+import firebase from 'firebase';
 import {
   EMAIL_CHANGED,
-  PASSWORD_CHANGED
+  PASSWORD_CHANGED,
+  LOGIN_SUCCESS
 } from './types';
 
 export const emailChanged = (email) => {
@@ -14,5 +16,17 @@ export const passwordChanged = (password) => {
   return {
     type: PASSWORD_CHANGED,
     payload: password
+  };
+};
+
+export const loginUser = ({ email, password }) => {
+  return (dispatch) => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(user => {
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: user
+      });
+    });
   };
 };
